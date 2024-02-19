078587e3a221dbf58c293cc2b843739d07aac22b
Aleksei Erchenko <proger@a1exxuss.ru>
2024-02-19 21:10:03 +0300

    feature/YH-23: add symlink for local testing package

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -4,5 +4,6 @@ import {BuildOptions} from "./types/types";
 export function buildResolvers(options: BuildOptions): Configuration['resolve'] {
     return {
         extensions: ['.tsx', '.ts'],
+        symlinks: false,
     }
 }
--- config/build/buildWebpack.ts
+++ config/build/buildWebpack.ts
@@ -25,6 +25,6 @@ export function buildWebpack(options: BuildOptions): webpack.Configuration {
     resolve: buildResolvers(options),
     externals: {
       react: 'react'
-    }
+    },
   };
 }
