7c03d0e364a9d73292f8332abcb64451ba4dbe73
Aleksei Erchenko <proger@a1exxuss.ru>
2024-02-19 21:59:08 +0300

    feature: YH-23 add husky

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -7,20 +7,15 @@ interface ButtonProps {
   label: string;
   onClick?: () => void;
 }
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
+export const Button = ({ primary = false, size = 'medium', backgroundColor, label, ...props }: ButtonProps) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
     <button
       type="button"
       className={[styles['storybook-button'], styles[`storybook-button--${size}`], mode].join(' ')}
       style={{ backgroundColor }}
-      {...props}>
+      {...props}
+    >
       {label}
     </button>
   );
