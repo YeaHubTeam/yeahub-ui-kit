b816fe1972657d43b10b29baa1f33d1432202457
Siarhei Shapavalau <Siarhei.Shapavalau@ventionteams.com>
2024-03-26 16:00:52 +0100

    feature/yh-52: add Button component

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { Button } from '../Button/Button';
+import { Button } from './Button';
+import React from 'react';
 
 const meta = {
   title: 'Components/Button',
@@ -8,9 +9,7 @@ const meta = {
     layout: 'centered',
   },
   tags: ['autodocs'],
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+  argTypes: {},
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -18,27 +17,78 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: 'Button',
+    theme: 'primary',
   },
+  render: () => (
+    <>
+      <Button value="Primary" />
+      <br />
+      <Button value="Primary with Icon" /* icon={AddIcon} */ />
+      <br />
+      <Button /* icon={AddIcon} */ />
+      <br />
+      <Button value="Primary" /* icon={AddIcon} */ disabled />
+      <br />
+      <Button value="Primary" /* icon={AddIcon} */ isActive />
+    </>
+  ),
 };
 
 export const Secondary: Story = {
   args: {
-    label: 'Button',
+    theme: 'secondary',
   },
+  render: (args) => (
+    <>
+      <Button value="Secondary" {...args} />
+      <br />
+      <Button value="Secondary with Icon" {...args} /* icon={AddIcon} */ />
+      <br />
+      <Button value="Secondary" {...args} /* icon={AddIcon} */ isActive />
+      <br />
+      <Button {...args} /* icon={AddIcon} */ />
+      <br />
+      <Button value="Secondary" {...args} /* icon={AddIcon} */ disabled />
+    </>
+  ),
 };
 
-export const Large: Story = {
+export const Destructive: Story = {
   args: {
-    size: 'large',
-    label: 'Button',
+    theme: 'destructive',
   },
+  render: (args) => (
+    <>
+      <Button value="Destructive" {...args} />
+      <br />
+      <Button value="Destructive with Icon" {...args} /* icon={AddIcon} */ />
+      <br />
+      <Button value="Destructive" {...args} /* icon={AddIcon} */ isActive />
+      <br />
+      <Button {...args} /* icon={AddIcon} */ />
+      <br />
+      <Button value="Destructive" {...args} /* icon={AddIcon} */ disabled />
+    </>
+  ),
 };
 
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
+export const Badge: Story = {
+  render: () => (
+    <>
+      <Button theme="secondary" value="Badge" badge={11} />
+      <br />
+      <Button
+        theme="primary"
+        value="Badge with Icon"
+        badge={10}
+        /* icon={AddIcon} */
+      />
+      <br />
+      <Button theme="destructive" value="Badge" badge={9} /* icon={AddIcon} */ isActive />
+      <br />
+      <Button theme="primary" badge={0} /* icon={AddIcon} */ />
+      <br />
+      <Button theme="primary" value="Badge" badge="text" /* icon={AddIcon} */ disabled />
+    </>
+  ),
 };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,26 +1,57 @@
-interface ButtonProps {
-  primary?: boolean;
-  backgroundColor?: string;
-  size?: 'small' | 'medium' | 'large';
-  label: string;
-  onClick?: () => void;
-}
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  return (
-    <button
-      type="button"
-      style={{ backgroundColor }}
-      {...props}
-      className={`storybook-button storybook-button--${size} ${mode}`}
-    >
-      {label}
-    </button>
-  );
-};
+import classnames from 'classnames';
+import React, { forwardRef, useCallback, memo, FC } from 'react';
+
+import { ButtonTypes } from './types';
+import { prefix } from '../../prefix';
+
+const compPrefix = `${prefix}-button`;
+
+export const Button: FC<ButtonTypes> = memo(
+  forwardRef<HTMLButtonElement, ButtonTypes>(
+    (
+      {
+        theme = 'primary',
+        value,
+        textClassName,
+        onClick,
+        isActive,
+        className,
+        iconLeft: IconLeft,
+        iconRight: IconRight,
+        iconClassName,
+        badge,
+        disabled,
+        ...otherProps
+      },
+      ref
+    ): JSX.Element => {
+      const handleClick = useCallback(
+        (e: React.MouseEvent<HTMLButtonElement>) => {
+          e.persist();
+          // prevent click when target is children
+          if (!disabled) onClick?.(e);
+        },
+        [onClick, disabled]
+      );
+
+      return (
+        <button
+          ref={ref}
+          disabled={disabled}
+          onClick={handleClick}
+          className={classnames(`${compPrefix}`, `${compPrefix}-${[theme]}`, className, {
+            [`${compPrefix}-is-active`]: isActive,
+          })}
+          tabIndex={1}
+          type="button"
+          {...otherProps}
+        >
+          {IconLeft && <IconLeft className={iconClassName} />}
+          {value && <span className={textClassName}>{value}</span>}
+          {IconRight && <IconRight className={iconClassName} />}
+          {(badge || badge === 0) && <div className={`${compPrefix}-badge`}>{badge}</div>}
+        </button>
+      );
+    }
+  )
+);
