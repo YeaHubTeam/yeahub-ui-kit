029f1c0bb7f1706e0e5570094d41fedcb5efc45f
Siarhei Shapavalau <Siarhei.Shapavalau@ventionteams.com>
2024-04-06 14:30:02 +0200

    feature/yh-52: update button component to match the UI

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,45 +1,58 @@
 import classnames from 'classnames';
 import React, { forwardRef, memo } from 'react';
 
-import { ButtonTypes } from './types';
+import { ButtonProps } from './types';
 import { prefix } from '../../prefix';
 
 const compPrefix = `${prefix}-button`;
+const defaultTag = 'button';
 
 export const Button = memo(
-  forwardRef<HTMLButtonElement, ButtonTypes>(
+  forwardRef<HTMLButtonElement | HTMLAnchorElement, ButtonProps>(
     (
       {
         theme = 'primary',
+        size = 'medium',
         value,
         textClassName,
         isActive,
         className,
-        iconLeft: IconLeft,
-        iconRight: IconRight,
-        iconClassName,
+        preffix,
+        suffix,
         badge,
+        type = 'button',
+        tagName = defaultTag,
         disabled,
+        'aria-label': ariaLabel,
         ...otherProps
       },
       ref
     ): JSX.Element => {
+      const Component = tagName as React.ElementType;
+
       return (
-        <button
+        <Component
           ref={ref}
+          className={classnames(
+            `${compPrefix}`,
+            ` ${compPrefix}-${[theme]} ${compPrefix}-${[size]}`,
+            className,
+            {
+              [`is-active`]: isActive,
+              [`is-square`]: !value,
+            }
+          )}
           disabled={disabled}
-          className={classnames(`${compPrefix}`, `${compPrefix}-${[theme]}`, className, {
-            [`${compPrefix}-is-active`]: isActive,
-          })}
-          tabIndex={1}
-          type="button"
+          aria-disabled={disabled}
+          type={type}
+          {...(!value ? { 'aria-label': ariaLabel } : {})}
           {...otherProps}
         >
-          {IconLeft && <IconLeft className={iconClassName} />}
+          {preffix}
           {value && <span className={textClassName}>{value}</span>}
-          {IconRight && <IconRight className={iconClassName} />}
+          {suffix}
           {(badge || badge === 0) && <div className={`${compPrefix}-badge`}>{badge}</div>}
-        </button>
+        </Component>
       );
     }
   )
