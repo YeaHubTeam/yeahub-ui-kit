0e327c9e65befcdb479ce3256d0d31f1aac8a727
Serhio46 <70842997+Serhio46@users.noreply.github.com>
2024-04-29 22:32:04 +0200

    fix/export: fix components reexport (#15)
    Co-authored-by: Siarhei Shapavalau <Siarhei.Shapavalau@ventionteams.com>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
 import './scss/main.scss';
-import { Button } from './components/Button/Button';
+import { Button } from './components/Button';
+import { FormControl } from './components/FormControl';
+import { Icon } from './components/Icon';
+import { IconButton } from './components/IconButton';
+import { Input } from './components/Input';
+import { Label } from './components/Label';
+import { Text } from './components/Text';
+import { TextArea } from './components/TextArea';
+import { Checkbox } from './components/toggles/Checkbox';
+import { Switch } from './components/toggles/Switch';
 
-export { Button };
+export {
+  Button,
+  FormControl,
+  Icon,
+  IconButton,
+  Input,
+  Label,
+  Text,
+  TextArea,
+  Checkbox,
+  Switch,
+};
