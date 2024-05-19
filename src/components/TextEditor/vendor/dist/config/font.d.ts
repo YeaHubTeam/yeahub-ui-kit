674ea99b5e33da0f7202507213d937dc0f655f1a
Siarhei Shapavalau <Siarhei.Shapavalau@ventionteams.com>
2024-05-19 11:14:39 +0200

    feat/YH-140: add declaration to build

---
+++ src/components/TextEditor/vendor/dist/config/font.d.ts
@@ -0,0 +1,18 @@
+export declare const fontColor: {
+  colors: (
+    | {
+        color: string;
+        hasBorder?: undefined;
+      }
+    | {
+        color: string;
+        hasBorder: boolean;
+      }
+  )[];
+  columns: number;
+  documentColors: number;
+};
+export declare const fontSize: {
+  options: (string | number)[];
+  supportAllValues: boolean;
+};
---
+++ src/components/TextEditor/vendor/dist/config/plugins.d.ts
@@ -0,0 +1,2 @@
+import type { Editor, PluginConstructor } from '@ckeditor/ckeditor5-core';
+export declare const plugins: PluginConstructor<Editor>[];
