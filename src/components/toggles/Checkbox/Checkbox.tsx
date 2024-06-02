0a6d08d6dc4ff2a06e86da877b9b298689e559ab
Aleksei Erchenko <proger@a1exxuss.ru>
2024-06-02 22:17:58 +0300

    fix: fix checkbox

--- src/components/toggles/Checkbox/Checkbox.tsx
+++ src/components/toggles/Checkbox/Checkbox.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, forwardRef } from 'react';
 
 import { prefix } from '../../../prefix';
 import { CheckboxProps } from './types';
@@ -6,30 +6,33 @@ import cn from 'classnames';
 import { Icon } from '../../Icon/Icon';
 const compPrefix = `${prefix}-checkbox`;
 
-export const Checkbox: FC<CheckboxProps> = ({
-  onToggle,
-  checked,
-  className = '',
-  disabled = false,
-  label,
-}) => {
-  const handleToggle = () => {
-    onToggle?.(!checked);
-  };
+export const Checkbox: FC<CheckboxProps> = forwardRef<HTMLInputElement, CheckboxProps>(
+  (
+    { onToggle, checked, className = '', disabled = false, label, ...otherProps },
+    ref
+  ) => {
+    const handleToggle = () => {
+      onToggle?.(!checked);
+    };
 
-  return (
-    <label className={cn(`${compPrefix}__wrapper`)}>
-      <input
-        type="checkbox"
-        className={`${compPrefix}_input`}
-        onChange={handleToggle}
-        checked={checked}
-        disabled={disabled}
-      />
-      <span className={cn(compPrefix, className)}>
-        {checked && <Icon icon="check" size={20} className={`${compPrefix}_icon`} />}
-      </span>
-      {label && <span className={`${compPrefix}_text`}>{label}</span>}
-    </label>
-  );
-};
+    return (
+      <label className={cn(`${compPrefix}__wrapper`)}>
+        <input
+          type="checkbox"
+          className={`${compPrefix}_input`}
+          onChange={handleToggle}
+          checked={checked}
+          disabled={disabled}
+          ref={ref}
+          {...otherProps}
+        />
+        <span className={cn(compPrefix, className)}>
+          {checked && <Icon icon="check" size={20} className={`${compPrefix}_icon`} />}
+        </span>
+        {label && <span className={`${compPrefix}_text`}>{label}</span>}
+      </label>
+    );
+  }
+);
+
+Checkbox.displayName = 'Checkbox';
