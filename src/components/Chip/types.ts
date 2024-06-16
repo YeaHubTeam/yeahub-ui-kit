ab7136c964d161c503387fb1c29d0b4f3738fea7
Serhio46 <70842997+Serhio46@users.noreply.github.com>
2024-06-16 13:00:56 +0200

    Fix chip (#52)
    * bugfix: fix onDelete hancler
    
    * bugfix: fix onDelete hancler
    
    ---------
    
    Co-authored-by: Siarhei Shapavalau <Siarhei.Shapavalau@ventionteams.com>

--- src/components/Chip/types.ts
+++ src/components/Chip/types.ts
@@ -1,7 +1,7 @@
 export interface ChipProps extends React.ComponentPropsWithRef<'div'> {
   theme: 'primary' | 'outlined';
   label?: string;
-  onDelete?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  onDelete?: () => void;
   disabled?: boolean;
   square?: boolean;
   active?: boolean;
