5cd07190dcd4de64f77b9cf286f13cd06327d5d6
A1exxuss <32575876+dedushkaalex@users.noreply.github.com>
2024-06-16 18:53:48 +0300

    Develop (#59)

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,10 +1,12 @@
 import { prefix } from '../../prefix';
 import { FC, useMemo, useRef } from 'react';
-import { Option, SingleSelectProps } from './types';
+import { MultiSelectProps, Option, SingleSelectProps } from './types';
 import { useSelect } from './hooks/useSelect';
 import cn from 'classnames';
 import { Popover } from '../Popover';
 import { Icon } from '../Icon';
+import { Checkbox } from '../toggles/Checkbox';
+import { useMultiSelect } from './hooks/useMultiSelect';
 
 const compPrefix = `${prefix}-select`;
 
@@ -15,6 +17,7 @@ const CustomSelect: FC<SingleSelectProps> = ({
   options,
   clearable,
   placeholder,
+  className,
 }) => {
   const {
     closeMenu,
@@ -99,7 +102,7 @@ const CustomSelect: FC<SingleSelectProps> = ({
   );
 
   return (
-    <div ref={ref} className={compPrefix}>
+    <div ref={ref} className={cn(compPrefix, className)}>
       <Popover
         onClickOutside={closeMenu}
         placement={'bottom'}
@@ -148,8 +151,161 @@ const CustomSelect: FC<SingleSelectProps> = ({
   );
 };
 
-export const Select = ({ ...props }: SingleSelectProps) => {
+const CustomMultiSelect: FC<MultiSelectProps> = (props) => {
+  const { closeMenu, isMenuOpen, onClear, onKeyDown, onKeyDownClear, ref, toggleMenu } =
+    useMultiSelect({
+      value: props.value,
+      onChange: props.onChange,
+      disabled: props.disabled,
+    });
+
+  const renderMenuOption = (option: Option, index: number) => {
+    const onKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+      if (e.key === ' ') {
+        onClick(e);
+      }
+    };
+
+    const onClick = (
+      e?: React.MouseEvent<HTMLDivElement> | React.KeyboardEvent<HTMLDivElement>
+    ) => {
+      e?.preventDefault();
+      if (props.value.includes(option.value)) {
+        props.onChange(props.value.filter((v) => v !== option.value));
+      } else {
+        props.onChange([...props.value, option.value]);
+      }
+    };
+
+    return (
+      <div
+        onKeyDown={onKeyDown}
+        key={index}
+        className={`${compPrefix}-menu-option ${compPrefix}-interactable`}
+        onClick={onClick}
+      >
+        <Checkbox
+          onToggle={() => onClick()}
+          checked={props.value.includes(option.value)}
+          label={option.label}
+        />
+      </div>
+    );
+  };
+
+  const selectWrapperClassNames = cn(`${compPrefix}-wrapper`);
+
+  const containerClassNames = cn(`${compPrefix}-container`, {
+    [`${prefix}-disabled`]: props.disabled,
+  });
+
+  const menuContent = useMemo(() => {
+    return (
+      <div
+        className={`${compPrefix}-menu`}
+        style={{ width: `${ref.current?.offsetWidth}px` }}
+      >
+        {props.options.map((option, index) => {
+          return renderMenuOption(option, index);
+        })}
+      </div>
+    );
+  }, [props.options, props.value, props.disabled, ref.current?.offsetWidth]);
+
+  const values = useMemo(() => {
+    const valueClassNames = cn(
+      `${compPrefix}-value`,
+      props.disabled ? `${compPrefix}-disabled` : ''
+    );
+
+    const clearValueContainerClassNames = cn(
+      `${compPrefix}-clear-value-container`,
+      props.disabled ? `${compPrefix}-disabled` : `${compPrefix}-interactable`
+    );
+
+    return (
+      <>
+        {props.value.map((value, index) => {
+          const allOptions = props.options.map((option) => option).flat();
+          const option = allOptions.find((option) => option.value === value);
+          if (option) {
+            return (
+              <div key={index} className={valueClassNames} onClick={toggleMenu}>
+                <div
+                  className={`${compPrefix}-value-prefix-wrapper ${compPrefix}-interactable`}
+                >
+                  <div className={`${compPrefix}-value-label`}>{option.label}</div>
+                </div>
+                <div
+                  onKeyDown={onKeyDownClear(option.value)}
+                  tabIndex={props.disabled ? undefined : 0}
+                  className={clearValueContainerClassNames}
+                  onClick={onClear(option.value)}
+                >
+                  <Icon icon="x" size={20} />
+                </div>
+              </div>
+            );
+          }
+        })}
+      </>
+    );
+  }, [props.value, props.options, props.disabled]);
+
+  const chevronContainerClassNames = cn(
+    `${compPrefix}-custom-chevron-container`,
+    props.disabled ? `${compPrefix}-disabled` : `${compPrefix}-interactable`
+  );
+
+  const multiSelectClassNames = cn(
+    compPrefix,
+    `${compPrefix}-multi`,
+    {
+      [`${compPrefix}-fixed-height`]: props.fixedHeight,
+      [`${compPrefix}-with-value`]: props.value.length > 1,
+    },
+    props.className
+  );
+
+  return (
+    <div ref={ref} className={multiSelectClassNames}>
+      <Popover
+        onClickOutside={closeMenu}
+        placement={'bottom'}
+        isOpen={isMenuOpen}
+        body={menuContent}
+      >
+        <div className={containerClassNames}>
+          <div
+            className={`${selectWrapperClassNames} ${props.value.length <= 1 ? `${compPrefix}-interactable` : ''}`}
+            onClick={() => !props.value.length && toggleMenu()}
+          >
+            {props.value.length < 2 ? (
+              <div onClick={() => toggleMenu()} className={`${compPrefix}-placeholder`}>
+                {props.placeholder}
+              </div>
+            ) : null}
+            {values}
+          </div>
+          <div
+            onClick={toggleMenu}
+            onKeyDown={onKeyDown}
+            tabIndex={props.disabled ? undefined : 0}
+            className={chevronContainerClassNames}
+          >
+            <Icon icon="caretDown" size={20} />
+          </div>
+        </div>
+      </Popover>
+    </div>
+  );
+};
+
+export const Select = ({ ...props }: SingleSelectProps | MultiSelectProps) => {
   if (props.type === 'default') {
     return <CustomSelect {...props} />;
+  }
+  if (props.type === 'multi') {
+    return <CustomMultiSelect {...props} />;
   }
 };
